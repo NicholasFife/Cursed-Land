b8724accbf53d291b420d0a66dfe6a7fc33e4b99
NicholasFife <69271435+NicholasFife@users.noreply.github.com>
2021-06-13 18:01:59 -0400

    Add files via upload

---
+++ Waypoints/Waypoint_Manager.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint_Manager : MonoBehaviour
+{
+    [SerializeField]
+    Waypoint_Single[] Ways;
+    public LayerMask obstacles;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Ways = GetComponentsInChildren<Waypoint_Single>();
+        //Reset rotation just in case
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        for (int i = 0; i <= Ways.Length - 1; i++)
+        {
+            //These need to reset to default each time a new waypoint starts assigning variables
+            float northDistance = 1000;
+            float westDistance = 1000;
+            float eastDistance = 1000;
+            float southDistance = 1000;
+
+            for (int b = 0; b <= Ways.Length - 1; b++)
+            {
+                //Check Distance & Find DotProduct
+                float distance = Vector3.Distance(Ways[i].transform.position, Ways[b].transform.position);
+
+
+                Vector3 targetDir = Ways[b].transform.position - Ways[i].transform.position;
+                targetDir = targetDir.normalized;
+                float dotProduct = Vector3.Dot(Ways[i].transform.forward, targetDir);
+                
+                //Debug.Log("Waypoint " + i + " checked against waypoint " + b + " dotproduct is " + dotProduct + ". Distance was " + distance);
+                //Check North
+                if(Ways[i] == Ways[b])
+                {
+                    //Debug.Log("Cannot check waypoint against self");
+                }
+
+                else if (dotProduct > .5)
+                {
+                    //Debug.Log("Waypoint identified as North");
+                    //see if the new waypoint is closer
+                    if(distance < northDistance)
+                    {
+                       // Debug.Log("Waypoint Identified as closer than currently selected waypoint by " + (distance - northDistance));
+                        if(CheckLOS(Ways[i].transform, Ways[b].transform))
+                        {
+                            //Debug.Log("Waypoint " + Ways[b] + " was identified as being closer to " + Ways[i] + " than previously selected waypoint");
+
+                            Ways[i].SetNorth(Ways[b]);
+                            northDistance = distance;
+                        }
+                    }
+                }
+                //Check West && East
+                else if (dotProduct < .5f && dotProduct > -.5f)
+                {
+                    //West
+                    if (Ways[i].transform.position.x > Ways[b].transform.position.x)
+                    {
+                        if (distance < westDistance)
+                        {
+                            //Debug.Log("Waypoint identified as West");
+                            //see if the new waypoint is closer
+                            if (distance < westDistance)
+                            {
+                                //Debug.Log("Waypoint Identified as closer than currently selected waypoint by " + (distance - westDistance));
+                                if (CheckLOS(Ways[i].transform, Ways[b].transform))
+                                {
+                                    //Debug.Log("Waypoint " + Ways[b] + " was identified as being closer to " + Ways[i] + " than previously selected waypoint");
+
+                                    Ways[i].SetWest(Ways[b]);
+                                    westDistance = distance;
+                                }
+                            }
+                        }
+                    }
+                    //East
+                    else if (Ways[i].transform.position.x < Ways[b].transform.position.x)
+                    {
+                        if (distance < eastDistance)
+                        {
+                            //Debug.Log("Waypoint identified as East");
+                            //see if the new waypoint is closer
+                            if (distance < eastDistance)
+                            {
+                                //Debug.Log("Waypoint Identified as closer than currently selected waypoint by " + (distance - eastDistance));
+                                if (CheckLOS(Ways[i].transform, Ways[b].transform))
+                                {
+                                    //Debug.Log("Waypoint " + Ways[b] + " was identified as being closer to " + Ways[i] + " than previously selected waypoint");
+
+                                    Ways[i].SetEast(Ways[b]);
+                                    eastDistance = distance;
+                                }
+                            }
+                        }
+                    }
+                }
+                //Check South
+                else //Object must be south by elimination
+                {
+                    if (distance < southDistance)
+                    {
+                        //Debug.Log("Waypoint identified as South");
+                        //see if the new waypoint is closer
+                        if (distance < southDistance)
+                        {
+                            //Debug.Log("Waypoint Identified as closer than currently selected waypoint by " + (distance - southDistance));
+                            if (CheckLOS(Ways[i].transform, Ways[b].transform))
+                            {
+                                //Debug.Log("Waypoint " + Ways[b] + " was identified as being closer to " + Ways[i] + " than previously selected waypoint");
+
+                                Ways[i].SetSouth(Ways[b]);
+                                southDistance = distance;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public virtual bool CheckLOS(Transform one, Transform two)
+    {
+        Vector3 toTarget = two.position - one.position;
+        float distance = Vector3.Distance(one.position, two.position);
+        RaycastHit hit;
+        if (!Physics.Raycast(one.position, toTarget, out hit, distance, obstacles))
+        { return true; }
+        return false;
+    }
+
+    public Waypoint_Single AssignStartPOS(Transform enemyT)
+    {
+        float savedDistance = 1000;
+        Waypoint_Single savedWaypoint;
+        savedWaypoint = Ways[0];
+        for (int i = 0; i <= Ways.Length -1; i++)
+        {
+            float thisDistance = Vector3.Distance(Ways[i].transform.position, enemyT.position);
+            if (thisDistance < savedDistance)
+            {
+                savedWaypoint = Ways[i];
+                savedDistance = thisDistance;
+            }
+        }
+
+        Debug.Log("Enemy at coordinates: " + enemyT.position + " is being assigned waypoint named: " + savedWaypoint.name + "at coordinates: " + savedWaypoint.transform.position + " as their reference to find a new destination");
+        return savedWaypoint;
+    }
+}
---
+++ Waypoints/Waypoint_Single.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint_Single : MonoBehaviour
+{
+    public Waypoint_Single[] neighbors= new Waypoint_Single[4];
+    bool singleReturn;
+    Transform playerT;
+    
+    int totalNeighbors = 4;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        playerT = FindObjectOfType<Entity_Player>().transform;
+    }
+
+    void Start()
+    {
+
+        for (int i = 0; i <= neighbors.Length - 1; i++)
+        {
+            if(neighbors[i] == null)
+            {
+                totalNeighbors--;
+            }
+        }
+        if (totalNeighbors == 1)
+        { singleReturn = true; }
+    }
+
+    void Update()
+    {
+        DebugLines();
+    }
+    
+    public Waypoint_Single GetWaypoint(Waypoint_Single lastWaypoint)
+    {
+        if (singleReturn)//If true, GetWaypoint will return before completing the rest of the method
+        {
+            for (int i = 0; i <= neighbors.Length-1; i++)
+            {
+                if(neighbors[i] != null)
+                { return neighbors[i]; }
+            }
+            //Just in case my code isn't working as expected, this should catch it
+            Debug.LogError("GetWaypoint singleReturn Error: lastWaypoint repeating to recover");
+            return lastWaypoint;
+        }
+
+        //Generate a number within appropriate range, excluding 1 for the last waypoint which should not repeat
+        int randdir = Random.Range(0, totalNeighbors-1);
+        for (int i = 0; i <= neighbors.Length -1; i++)
+        {
+            //check if it is valid and needs accounted for in the count
+            if(neighbors[i] != null && neighbors[i] != lastWaypoint)
+            {
+                if (randdir == 0)
+                {
+                    //Debug.Log("GetWaypoint has worked correctly, returning the waypoint in array slot: " + i);
+                    return neighbors[i];
+                }
+                else randdir--;
+                
+                //Used to recover in case something goes wrong - *shouldn't* ever run...
+                if(randdir < 0)
+                {
+                    Debug.LogError("Waypoint_Single.GetWaypoint counted past the desired waypoint: lastWaypoint repeating to recover");
+                    return lastWaypoint;
+                }
+            }
+        }
+        Debug.LogError("GetWaypoint reached end of method without returning an appropriate waypoint: lastWaypoint repeating to recover");
+        return lastWaypoint;
+    }
+
+    //Called by Enemy when exiting
+    public Waypoint_Single FindWaypointToPlayer()
+    {
+        Vector3 targetDir = playerT.position - transform.position;
+        targetDir = targetDir.normalized;
+        float dotProduct = Vector3.Dot(transform.forward, targetDir);
+
+        return CardinalToPlayer(dotProduct);
+    }
+
+    //Returns a reference to whichever point is in the correct direction
+    Waypoint_Single CardinalToPlayer(float dot)
+    {
+        if (dot > .5f) //Player is North
+        {
+            Debug.Log("Player was north of " + gameObject.name);
+           
+            return neighbors[0];
+
+        }
+        else if (dot < .5f && dot > -.5f)//Player is East or West
+        {
+            if (transform.position.x > playerT.position.x) //Player is west
+            {
+                Debug.Log("Player was west of " + gameObject.name);
+
+
+                    return neighbors[1];
+                
+            }
+            else //Player is east
+            {
+                Debug.Log("Player was east of " + gameObject.name);
+
+                    return neighbors[2];
+
+            }
+        }
+        else //player is south
+        {
+            Debug.Log("Player was south of " + gameObject.name);
+
+                return neighbors[3];
+            
+        }
+    }
+
+    public Waypoint_Single OnCardinalToPlayerFail(Transform enemy)
+    {
+        Vector3 targetDir = enemy.position - transform.position;
+        targetDir = targetDir.normalized;
+        float dotProduct = Vector3.Dot(transform.forward, targetDir);
+        dotProduct = -dotProduct;
+
+
+        if (dotProduct > .5f)
+        {
+            Debug.Log("Player was north of " + gameObject.name);
+
+            if (neighbors[0] != null)
+            {
+                return neighbors[0];
+            }
+
+        }
+        else if (dotProduct < .5f && dotProduct > -.5f)
+        {
+            if (transform.position.x > (enemy.position.x * -1)) 
+            {
+                Debug.Log("Player was west of " + gameObject.name);
+
+                if (neighbors[1] != null)
+                {
+                    return neighbors[1];
+                }
+
+            }
+            else
+            {
+                Debug.Log("Player was east of " + gameObject.name);
+                if (neighbors[2] != null)
+                {
+                    return neighbors[2];
+                }
+
+            }
+        }
+        else //player is south
+        {
+            Debug.Log("Player was south of " + gameObject.name);
+            if (neighbors[3] != null)
+            {
+                return neighbors[3];
+            }
+        }
+        return GetComponent<Waypoint_Single>();
+    }
+
+    //Used to set neighbor references
+    public void SetNorth(Waypoint_Single n)
+    {
+        neighbors[0] = n;
+    }
+    public void SetWest(Waypoint_Single n)
+    {
+        neighbors[1] = n;
+    }
+    public void SetEast(Waypoint_Single n)
+    {
+        neighbors[2] = n;
+    }
+    public void SetSouth(Waypoint_Single n)
+    {
+        neighbors[3] = n;
+    }
+
+    //Activate to draw lines between point connections
+    void DebugLines()
+    {
+        if (neighbors[0] != null)
+        { Debug.DrawLine(transform.position, neighbors[0].transform.position); }
+        if (neighbors[1] != null)
+        { Debug.DrawLine(transform.position, neighbors[1].transform.position); }
+        if (neighbors[2] != null)
+        { Debug.DrawLine(transform.position, neighbors[2].transform.position); }
+        if (neighbors[3] != null)
+        { Debug.DrawLine(transform.position, neighbors[3].transform.position); }
+    }
+}
